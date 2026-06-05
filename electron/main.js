dec907e7eb53afc19ae48a8a891aac1caa194c04
solomon17705cs <solomon17705@gmail.com>
2026-06-05 22:49:02 +0530

    Added Desktop
    Now the app is ready for desktops with the help of the electron

---
+++ electron/main.js
@@ -0,0 +1,77 @@
+const { app, BrowserWindow } = require('electron');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const distPath = path.join(__dirname, '..', 'dist');
+
+const MIME_TYPES = {
+  '.html': 'text/html',
+  '.js': 'application/javascript',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+  '.json': 'application/json',
+  '.woff': 'font/woff',
+  '.woff2': 'font/woff2',
+  '.ttf': 'font/ttf',
+  '.map': 'application/json',
+};
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(distPath, req.url === '/' ? 'index.html' : req.url);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not Found');
+      return;
+    }
+    const ext = path.extname(filePath).toLowerCase();
+    res.writeHead(200, { 'Content-Type': MIME_TYPES[ext] || 'application/octet-stream' });
+    res.end(data);
+  });
+});
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+    title: 'KGS Music Academy',
+    icon: path.join(distPath, 'favicon.ico'),
+  });
+
+  mainWindow.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(() => {
+  server.listen(5173, () => {
+    console.log('KGS Music Academy running on http://localhost:5173');
+    createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    server.close();
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
